40c45eefd9e8ba2db9dd1dff3e69d3c4888e8277
cucinelli giulio <157374392+cucinelliGiulio@users.noreply.github.com>
2024-11-13 16:27:02 +0100

    Main.java

--- Main.java
+++ Main.java
@@ -2,35 +2,30 @@ import java.util.in;
 
 class Main{
     public static void main(String args[]){
-        //Istanziare uno scaffale
-        
-        
-        //Istanziare due oggetti di classe Libro e aggiungerli allo scaffale
-        
-        //Visualizzare il contenuto dello scaffale
+
         
         class scaffale = new class scaffalenew;
             Scaffale(String id){
                 this.id = id;
                 libri = new Libro[N];
                 nLibri = 2;
-            };    
-            Public Boolean Aggiungi(Libro l){
+            }    
+            public boolean aggiungi(Libro l){
                 if (nLibri<N){
                     libri[nLibri] = l;
                     nLibri++;
                     return true;
-                };
+                }
                 return false;
-            };     
+            }     
             
-            Public void elencaLibri(){
+            public void elencaLibri(){
                 System.out.println("Scaffale: " + id);
                 for(Libro l: libri){
                     System.out.println(l):
-                };  
-            };
-        };
+                }    
+            }
+        }
         
         class Libro = new class Libronew{
             String Libronew
@@ -41,12 +36,13 @@ class Main{
             }
             
             
-        };
+        }
         public void elencaLibri(){
             System.out.println("Scaffale: " + id);
             for(Libro l: libri){
                 System.out.println(l);
-            };    
-        };
+            }    
+        }
         
-    };
+    }
+}
